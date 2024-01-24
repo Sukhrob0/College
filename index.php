5a8c044bac8a3b2c23c13246bba05fc943c54a43
ProjectMen05 <156096726+ProjectMen05@users.noreply.github.com>
2024-01-24 16:43:11 +0300

    PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<h1>Регистрация</h1>
+
+
+
+<form action="reg.php" method="POST">
+    <input type="text" name="login" placeholder="Введите логин">
+    <input type="password" name="password" placeholder="Введите паролоь">
+    <input type="submit" >
+
+</form>  
+<h1>Авторизация</h1>
+
+
+
+<form action="avto.php" method="POST">
+    <input type="text" name="login" placeholder="Введите логин">
+    <input type="password" name="password" placeholder="Введите паролоь">
+    <input type="submit" >
+
+</form> 
